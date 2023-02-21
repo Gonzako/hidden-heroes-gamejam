4d0ec44738874f00d1c67211eb74330b5adb7c56
Gonzako <gonzako123@gmail.com>
2023-02-21 19:46:26 +0100

    added small soundy implementation

---
+++ Assets/SoundyTest.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Doozy.Engine.Soundy;
+
+public class SoundyTest : MonoBehaviour
+{
+    public SoundyData TestData;
+}
