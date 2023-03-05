b87ac19ac1927f5f1fca2ae60e8b5539a056945f
Nielisson Mendonça <nielisson_silva@hotmail.com>
2023-03-05 22:20:56 +0100

    main menu

---
+++ Assets/Scripts/PlaySoundEffect.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundEffect : MonoBehaviour
+{
+    [SerializeField] AudioSource audioS;
+    [SerializeField] AudioClip woosh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioS = this.gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayWoosh()
+    {
+        audioS.PlayOneShot(woosh);
+    }
+}
